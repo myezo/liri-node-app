2f0e912ae952a2f5468a0213d45d4f2104fbbfaa
myezo <mikeyyz315@gmail.com>
2017-01-25 20:32:31 -0500

    first commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '5Pv0GGMbWpVy31eGso4oEEBud',
+  consumer_secret: 'T4uaRN3hOTVZg0TvXuV1CRzox2VCkmdkdbpv1C90vmD5UCkJSu',
+  access_token_key: '824326796711706624-okcdVYMU3FMjv7gR5lJ09Do4eBmEUwa',
+  access_token_secret: 'wP7MosVeILdzxi1CDuGh5yd7a554Oyp2TYmjJSBpf1EHE',
+}
+
+console.log(exports.twitterKeys);
